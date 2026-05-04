1cee6d10b8147277937d145cce369e5a9b80b6ab
monkberry <monkberrymd@gmail.com>
2026-05-04 17:14:42 +0900

    Add : ramdisk

---
+++ kernel/driver/ramdisk.c
@@ -0,0 +1,44 @@
+#include <stdint.h>
+#include "../fs.h"
+#include "string.h"
+
+static char ramdisk[DISK_SIZE];
+
+static uint64_t disk_size;
+
+void rd_init(void) {
+    disk_size = DISK_SIZE;
+}
+
+void rd_intr(void) {
+
+}
+
+void rd_rw(struct buf *b) {
+    // !!!!!!! if not locked, pannic
+    if((b->flags & (B_VALID | B_DIRTY)) == B_VALID) {
+        // valid but clean, why did you call me in the first place?
+        panic("rd_rw: nothing to do");
+    }
+
+    if(b->dev != DEV_RAMDISK) {
+        panic("rd_rw: request not for rd");
+    }
+
+    if(b->block_no >= disk_size) {
+        panic("rd_rw: block out of range");
+    }
+
+    char *p = ramdisk + b->block_no * BSIZE;
+
+    if(b->flags & B_DIRTY) {
+        // we got it written, so it is clean 
+        b->flags &= ~B_DIRTY;
+        memcpy(p, b->data, BSIZE);
+    } else {
+        // non_valid and clean -> read from the disk
+        memcpy(b->data, p, BSIZE);
+    }
+
+    b->flags |= B_VALID; // anyway, we synced it.
+}
---
+++ kernel/fs.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <stdint.h>
+
+#define BSIZE 4096
+#define DISK_SIZE (16 * 1024 * 1024)
+#define NBLOCKS (DISK_SIZE / BSIZE)
+
+#define B_VALID 0x2
+#define B_DIRTY 0x4
+
+#define DEV_RAMDISK 1
+
+struct buf {
+    uint64_t flags;
+    uint64_t dev;
+    uint64_t block_no;
+    // struct sleep_lock lk;
+    uint32_t ref_count;
+    struct buf *prev, *next, *qnext;
+    uint8_t data[BSIZE];
+};
